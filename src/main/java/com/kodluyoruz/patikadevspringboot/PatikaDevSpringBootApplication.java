498cd6ed8f1a4cbef09c05cb4612826b6424ac05
frattshn <frtshn907@gmail.com>
2022-04-09 21:11:13 +0300

    Initial commit

---
+++ src/main/java/com/kodluyoruz/patikadevspringboot/PatikaDevSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.kodluyoruz.patikadevspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatikaDevSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatikaDevSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodluyoruz/patikadevspringboot/PatikaDevSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodluyoruz.patikadevspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatikaDevSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
