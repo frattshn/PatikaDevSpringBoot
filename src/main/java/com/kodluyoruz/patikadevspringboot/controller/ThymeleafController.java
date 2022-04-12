871115616d62e7bd39017d6e5002f5dae6500339
frattshn <frtshn907@gmail.com>
2022-04-13 01:18:35 +0300

    Model object, Model list, thymeleaf if-else

--- src/main/java/com/kodluyoruz/patikadevspringboot/controller/ThymeleafController.java
+++ src/main/java/com/kodluyoruz/patikadevspringboot/controller/ThymeleafController.java
@@ -1,30 +1,62 @@
 package com.kodluyoruz.patikadevspringboot.controller;
 
+import com.kodluyoruz.patikadevspringboot.dto.ProductDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ThymeleafController {
 
     // http://localhost:8080/thymeleaf1
     @GetMapping("/thymeleaf1")
-    public String getTyhemeleaf1(){
+    public String getTyhemeleaf1() {
         return "thymeleaf1"; //templates klasöründeki thymeleaf1.html açılır
     }
 
     // http://localhost:8080/thymeleaf2
     @GetMapping("/thymeleaf2")
-    public String getThymeleaf2Model(Model model){
+    public String getThymeleaf2Model(Model model) {
         model.addAttribute("key_model", "From Model class");
         return "thymeleaf1";
     }
 
     // http://localhost:8080/thymeleaf3
     @GetMapping("/thymeleaf3")
-    public String getThymeleaf3Model(Model model){
+    public String getThymeleaf3Model(Model model) {
         model.addAttribute("key_model", "From Model class");
         return "thymeleaf2";
+    }
+
+    // Model Object gönderme
+    // http://localhost:8080/thymeleaf4
+    @GetMapping("/thymeleaf4")
+    public String getThymeleaf4Model(Model model) {
+        model.addAttribute("key_model1", "text");
+        ProductDto productDto = ProductDto
+                .builder()
+                .productID(0L)
+                .productName("Ürün adı")
+                .productPrice(2500)
+                .build();
+        model.addAttribute("key_model2", productDto);
+        return "thymeleaf3";
+    }
+
+    // Model Object List gönderme
+    // http://localhost:8080/thymeleaf5
+    @GetMapping("/thymeleaf5")
+    public String getThymeleaf5Model(Model model) {
+        model.addAttribute("key_model1", "text");
+        List<ProductDto> list = new ArrayList<>();
+        list.add(ProductDto.builder().productID(0L).productName("Ürün adı").productPrice(2500).build());
+        list.add(ProductDto.builder().productID(1L).productName("Ürün adı 2").productPrice(25).build());
+        list.add(ProductDto.builder().productID(2L).productName("Ürün adı 3").productPrice(5800).build());
+        model.addAttribute("productList", list);
+        return "thymeleaf4";
     }
 
 
