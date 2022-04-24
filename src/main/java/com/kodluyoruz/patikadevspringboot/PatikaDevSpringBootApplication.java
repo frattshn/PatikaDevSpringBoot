f03cc3e8997109cb9fe0e2baa37837bd7ea34898
frattshn <frtshn907@gmail.com>
2022-04-24 23:27:44 +0300

    Auditor

--- src/main/java/com/kodluyoruz/patikadevspringboot/PatikaDevSpringBootApplication.java
+++ src/main/java/com/kodluyoruz/patikadevspringboot/PatikaDevSpringBootApplication.java
@@ -2,11 +2,14 @@ package com.kodluyoruz.patikadevspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication(exclude = {
         org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
         org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
 })
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class PatikaDevSpringBootApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/kodluyoruz/patikadevspringboot/data/entity/BaseEntity.java
+++ src/main/java/com/kodluyoruz/patikadevspringboot/data/entity/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.kodluyoruz.patikadevspringboot.data.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
@@ -12,10 +13,14 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.*;
 import java.util.Date;
 
-@MappedSuperclass
+
 @Getter
 @Setter
+
+@MappedSuperclass
+
 @EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"created_date, updated_date"}, allowGetters = true)
 public class BaseEntity {
 
     @Id
